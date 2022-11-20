ce8d6a22ce19d333c808218ec78a9980d55cb855
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-11-20 16:23:01 +0700

    checkpoint. adding client feedback

--- models/models.py
+++ models/models.py
@@ -39,6 +39,10 @@ class TeamActivity(models.Model):
         _name = 'middle.logging.team.activity'
         _description = 'team activity'
 
+        # fake foreign key
+        team = fields.Selection(selection=[
+            ('1', 'tim 1'), ('2', 'tim 2'), ('3', 'tim 3'), ('4', 'tim 4'), ('5', 'tim 5')], string="team", required=True)
+            
         clientSatisfactionRate = fields.Integer(string="clientSatisfactionRate", required=True)
         taskWeightRate = fields.Integer(string="taskWeightRate", required=True)
         effectivenessRate = fields.Integer(string="effectivenessRate", required=True)
@@ -47,4 +51,32 @@ class TeamActivity(models.Model):
 
 
 
+class ClientFeedback(models.Model):
+        _name = 'middle.logging.client.feedback'
+        _description = 'client feedback'
+
+
+        mood = fields.Selection(selection=[
+            ('5', 'Sangat Puas'), ('4', 'Puas'), ('3', 'Biasa Saja'), ('2', 'Tidak Puas'), ('1', 'Sangat Tidak Puas'), 
+        ], string="Mood", required=True)
+        note = fields.Char(string="note", required=False)
+
+
+class WorkerActivity(models.Model):
+    _name = 'middle.logging.worker.activity'
+    _description = 'worker activity'
+
+    # fake foreign key
+    worker = fields.Selection(selection=[
+        ('1', 'worker 1'), ('2', 'worker 2'), ('3', 'worker 3'), ('4', 'worker 4'), ('5', 'worker 5'),
+    ], string="worker", required=True)
+
+    # data
+    activity = fields.Selection(selection=[
+        ('1', 'Menyapu'), ('2', 'Berkebun'), ('3','Mencuci' )]
+        , string="Activity", required=True)
+    startTime = fields.Datetime(string="Start Time", required=True)
+    endTime = fields.Datetime(string="End Time", required=True)
+    rate = fields.Integer(string="Rate", required=True)
+    note = fields.Char(string="Note", required=False)
 
